7182f7abb785b97364a4245fe92d1bb403727b32
HTsmuh <syedminhaj@horizontech.biz>
2017-08-06 13:36:32 +0500

    Get Json

--- app/src/main/java/cardiac/general/hospital/medicare/Specialities.java
+++ app/src/main/java/cardiac/general/hospital/medicare/Specialities.java
@@ -1,13 +1,73 @@
 package cardiac.general.hospital.medicare;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 
 public class Specialities extends AppCompatActivity {
+
+    String TAG = "Response";
+    SoapPrimitive resultString;
+    ListView Specialities_List;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_specialities);
+
+        Specialities_List= (ListView) findViewById(R.id.Specialities_list);
+
+        AsyncCallWS task = new AsyncCallWS();
+        task.execute();
+    }
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "onPreExecute");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.i(TAG, "doInBackground");
+            GetJSON();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i(TAG, "onPostExecute");
+            Toast.makeText(Specialities.this, "Response" + resultString.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+    public void GetJSON() {
+
+        String METHOD_NAME = "Get_Speciality";
+        String NAMESPACE = "http://medicarehospital.pk//";
+        String URL = "http://medicarehospital.pk/WebService.asmx";
+        String SOAP_ACTION = NAMESPACE+METHOD_NAME;
+
+        try {
+            SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            soapEnvelope.dotNet = true;
+            soapEnvelope.setOutputSoapObject(Request);
+
+            HttpTransportSE transport = new HttpTransportSE(URL);
+
+            transport.call(SOAP_ACTION, soapEnvelope);
+            resultString= (SoapPrimitive) soapEnvelope.getResponse();
+
+            Log.i(TAG, "Result: " + resultString);
+        } catch (Exception ex) {
+            Log.e(TAG, "Error: " + ex.getMessage());
+        }
     }
 }
